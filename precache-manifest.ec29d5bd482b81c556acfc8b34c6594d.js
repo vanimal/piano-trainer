c0d142beefdc58a997a286ef70d849c7f6db73ea
Van Olmstead <vanimal@gmail.com>
2020-04-22 17:13:04 -0500

    Updates

---
+++ precache-manifest.ec29d5bd482b81c556acfc8b34c6594d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90e2a868df9355e5b4ce352e1e7ed010",
+    "url": "/piano-trainer/index.html"
+  },
+  {
+    "revision": "004b7bda6cd3ded313ac",
+    "url": "/piano-trainer/static/css/main.32ddebcd.chunk.css"
+  },
+  {
+    "revision": "b32d4b16c8addff205a0",
+    "url": "/piano-trainer/static/js/2.88c809b5.chunk.js"
+  },
+  {
+    "revision": "3e06395ea99acadcc0cefbe6ed7e7627",
+    "url": "/piano-trainer/static/js/2.88c809b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "004b7bda6cd3ded313ac",
+    "url": "/piano-trainer/static/js/main.f8d400d0.chunk.js"
+  },
+  {
+    "revision": "91f3ff445308afd6ac25",
+    "url": "/piano-trainer/static/js/runtime-main.f59b4d8e.js"
+  }
+]);
